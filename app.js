3c26c22bfabf182c636f5df3a723b772e7543bca
Pastendi <pastendisherpa26@gmail.com>
2022-08-03 16:51:23 +0545

    Initail server setup

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+require('dotenv').config() //allows us to reach to the variable in .env file
+
+//middlewares
+//url just for checking if the server is working
+app.get('/', (req, res) => {
+  res.send('<h1>The server is working fine</h1>')
+})
+
+//port for server
+const port = process.env.PORT || 3000
+
+//setting the sever startup
+const startServer = async () => {
+  try {
+    app.listen(port, console.log(`Server is listening at port ${port}....`))
+  } catch (err) {
+    console.log(err)
+  }
+}
+startServer()
